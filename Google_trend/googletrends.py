c80677c4b5200db6af867c8614578bb36c4b93f3
sn0wfree <snowfreedom0815@gmail.com>
2017-03-07 13:28:57 +0000

    test

---
+++ Google_trend/googletrends.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+#-------------------
+__version__ = "0.8"
+__author__ = "sn0wfree"
+'''
+This tool is for connecting the Google Trends Service
+'''
+#-------------------
+import os
+import reuqests
+import gc
+import pandas as pd
