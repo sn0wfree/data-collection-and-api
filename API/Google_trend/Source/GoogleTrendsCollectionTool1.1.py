d726f96fcb2dd1be4f978aa3796ddc76089eaae9
sn0wfree <snowfreedom0815@gmail.com>
2017-09-15 15:18:27 +0100

    Gui setup

--- API/Google_trend/Source/GoogleTrendsCollectionTool1.1.py
+++ API/Google_trend/Source/GoogleTrendsCollectionTool1.1.py
@@ -7,7 +7,6 @@ import time
 import requests_cache
 
 
-
 import pandas as pd
 import re
 import gc
@@ -21,20 +20,19 @@ from datetime import datetime
 import requests
 
 
+def get_data(bucket_start_date, bucket_end_date, keywords, category=7):
+    geo = ''
 
-
-
-def get_data(bucket_start_date, bucket_end_date, keywords):
-    geo=''
-
-    bucket_start_date_printed = datetime.strftime(bucket_start_date, '%Y-%m-%d')
+    bucket_start_date_printed = datetime.strftime(
+        bucket_start_date, '%Y-%m-%d')
     bucket_end_date_printed = datetime.strftime(bucket_end_date, '%Y-%m-%d')
-    time_formatted = '+'.join([bucket_start_date_printed,bucket_end_date_printed])
-    comparisonItem = [{'keyword': kw,'time': time_formatted, 'geo': geo} for kw in keywords]
-    
-    
+    time_formatted = '+'.join([bucket_start_date_printed,
+                               bucket_end_date_printed])
+    comparisonItem = [
+        {'keyword': kw, 'time': time_formatted, 'geo': geo} for kw in keywords]
 
-    req = {"comparisonItem": comparisonItem,"category": 7, "property": ""}
+    req = {"comparisonItem": comparisonItem,
+           "category": category, "property": ""}
     hl = "en-GB"
     tz = "-120"
 
@@ -75,7 +73,6 @@ def get_csv(response_text):
 def parse_csv(csv_contents, keywords):
     lines = csv_contents.split('\n')
     datas = lines[3:-1]
-    
 
     col = lines[2].split(',')
     for colnumber in xrange(len(col)):
@@ -87,7 +84,7 @@ def parse_csv(csv_contents, keywords):
         locals()[col[colnumber]] = []
 
     df = pd.DataFrame(columns=col)
-    
+
     # Delete top 3 lines
     for data in datas:
         for columnnumber in xrange(len(col)):
@@ -96,7 +93,6 @@ def parse_csv(csv_contents, keywords):
     for columnnumber in xrange(len(col)):
         df[col[columnnumber]] = locals()[col[columnnumber]]
     return df
-
 
 
 def read_a_file(file):
@@ -130,15 +126,16 @@ def ImportListInfobyFile(locals_file_path, target=False, GUI=False):
             keywords = fi['tinker']
         else:
             keywords = 0
+            raise ValueError('unexpected keywords')
 
     return keywords
 
 
-def HandleGTs(start_date, end_date, keywords):
+def HandleGTs(start_date, end_date, keywords, category):
     start_date_dt = datetime.strptime(start_date, '%Y-%m-%d')
     end_date_dt = datetime.strptime(end_date, '%Y-%m-%d')
 
-    resp_text = get_data(start_date_dt, end_date_dt, keywords)
+    resp_text = get_data(start_date_dt, end_date_dt, keywords, category)
     # print resp_text
     df = parse_csv(get_csv(resp_text), keywords)
     return df
@@ -155,57 +152,125 @@ def findoperation():
     return link
 
 
-def DateProduce(beginyear):
+def DateProduce(begindate):
+    if len(begindate) == 4:
+        beginyear = begindate
+        return yearonly(beginyear)
+    elif len(begindate) == 6:
+        beginyear = begindate[0:4]
+        beginmonth = begindate[4:]
+    elif len(begindate) == 8:
+        beginyear = begindate[0:4]
+        beginmonth = begindate[4:6]
+        beginday = begindate[6:]
+
+
+def getDays(year, month):
+    year = int(year)
+    month = int(month)
+    day = 31  # 定义每月最多的天数
+    while day:
+        # print day
+        try:
+            time.strptime('%d-%d-%d' % (year, month, day),
+                          '%Y-%m-%d')  # 尝试将这个月最大的天数的字符串进行转化
+            break
+
+        except:
+            day -= 1  # 否则将天数减1继续尝试转化, 直到成功为止
+    return day  # 成功时返回得就是这个月的天数
+
+
+def yearandmonth(beginyear, beginmonth):
+    if int(beginyear) < datetime.now().year:
+        years = range(int(beginyear), datetime.now().year + 1, 1)
+        da = yearonly(int(beginyear) + 1)
+        if int(beginmonth) < 6:
+
+            da.append((str(beginyear) + '-01-01',
+                       str(beginyear) + '-0' + str(beginmonth) + '-' + str(getDays(beginyear, beginmonth))))
+        else:
+            if len(str(beginmonth)) = 1:
+                month = '0' + str(beginmonth)
+            else:
+                month = str(beginmonth)
+            da.append((str(beginyear) + '-01-01', str(beginyear) + '-06-30'))
+            da.append((str(beginyear) + '-07-01',
+                       str(beginyear) + '-' + month + '-' + str(getDays(beginyear, beginmonth))))
+
+    elif int(beginyear) == datetime.now().year:
+        years = [datetime.now().year]
+        if int(beginmonth) < datetime.now().month:
+
+        elif int(beginmonth) = datetime.now().month:
+            month = [datetime.now().month]
+        else:
+            raise ValueError('Wrong year, you choose the future month')
+
+    else:
+        raise ValueError('Wrong year, you choose the future year')
+
+
+def yearonly(beginyear):
     da = []
-    years = [beginyear + i for i in range(14)]
+    #years = [int(beginyear) + i for i in range(14)]
+
+    if int(beginyear) < datetime.now().year:
+        years = range(int(beginyear), datetime.now().year + 1, 1)
+    elif int(beginyear) == datetime.now().year:
+        years = [datetime.now().year]
+    else:
+        raise ValueError('Wrong year, you choose the future year')
+
     for year in years:
+        if year == datetime.now().year:
+            if datetime.now().month <= 6:
+                da.append((str(year) + '-01-01', str(year) +
+                           '-%d-%d'(datetime.now().month, datetime.now().day)))
+            else:
+                da.append((str(year) + '-01-01', str(year) + '-06-30'))
 
-        if year == 2017:
-            date1 = ('2017-05-01', '2017-06-30')
-            da.append(date1)
+                da.append((str(year) + '-07-01', str(year) +
+                           '-%d-%d'(datetime.now().month, datetime.now().day)))
 
-        elif year != 2017:
-            stryear = str(year)
-            if year % 2 == 1:
-                date1 = (stryear + '-05-01', stryear + '-12-31')
+        elif year < datetime.now().year:
+            da.append((str(year) + '-01-01', str(year) + '-06-30'))
+            da.append((str(year) + '-07-01', str(year) + '-12-31'))
+        else:
+            raise ValueError('Unexpected year, you choose the future year')
 
-                da.append(date1)
-
-            elif year % 2 == 0:
-                date1 = (stryear + '-01-01', stryear + '-08-31')
-                date2 = (stryear + '-09-01', str(year + 1) + '-04-30')
-                da.append(date1)
-                da.append(date2)
     return da
 
+
 def DateProduce_back(beginyear):
-    current_year =datetime.now().year
+    current_year = datetime.now().year
     if current_year < beginyear:
         raise 'Wrong year, the begin year is less than current year'
 
     else:
         pass
     da = []
-    years = xrange(beginyear,current_year+1)
+    years = xrange(beginyear, current_year + 1)
     for year in years:
 
         if year == current_year:
-            stryear =str(current_year)
+            stryear = str(current_year)
             if year % 2 == 1:
-                date1 = (stryear+'-05-01', datetime.now().date().strftime('%Y-%m-%d'))
+                date1 = (stryear + '-05-01',
+                         datetime.now().date().strftime('%Y-%m-%d'))
                 da.append(date1)
-
 
             elif year % 2 == 0:
                 if datetime.datetime.now().mouth < 9:
-                    date1 = (stryear + '-01-01', ddatetime.now().date().strftime('%Y-%m-%d'))
+                    date1 = (stryear + '-01-01',
+                             ddatetime.now().date().strftime('%Y-%m-%d'))
                     da.append(date1)
-                else: 
+                else:
                     date1 = (stryear + '-01-01', stryear + '-08-31')
-                    date2 = (stryear + '-09-01', datetime.now().date().strftime('%Y-%m-%d'))
+                    date2 = (stryear + '-09-01',
+                             datetime.now().date().strftime('%Y-%m-%d'))
                     da.append(date1)
                     da.append(date2)
-            
 
         elif year != current_year:
             stryear = str(year)
@@ -253,14 +318,14 @@ def write_txt(fi, targetpath):
             f.write(ff + '\n')
 
 
-def scraGoogleTrends_2yearPeriod(date, keywords, time2sleep=0.5):
-    df =[HandleGTs(dt[0], dt[1], keywords) for  dt in date]
-    
-    time.sleep(time2sleep)
-    
-    #return pd.concat([df_temp0, df_temp1, df_temp2])
-    return pd.concat(df)
+def scraGoogleTrends_2yearPeriod(date, keywords,  category=7):
+    if category == 7:
+        df = [HandleGTs(dt[0], dt[1], keywords, 7) for dt in date]
+    else:
+        df = [HandleGTs(dt[0], dt[1], keywords, category) for dt in date]
 
+    # return pd.concat([df_temp0, df_temp1, df_temp2])
+    return pd.concat(df)
 
 
 def makedirs(names, path, link):
@@ -280,14 +345,18 @@ def makedirs(names, path, link):
             pass
 
 
-def downlaod(dates, keywords, link, locals_file_path):
-
-    df_list = [scraGoogleTrends_2yearPeriod(date, keywords) for date in dates]
+def downlaod(dates, keywords, link, locals_file_path, category=7):
+    if category == 7:
+        df_list = [scraGoogleTrends_2yearPeriod(
+            date, keywords, 7) for date in dates]
+    else:
+        df_list = [scraGoogleTrends_2yearPeriod(
+            date, keywords, category) for date in dates]
 
     target_pd = pd.concat(df_list).T
 
     filename = ''.join(keywords)
-    
+
     savepath = locals_file_path + link + 'data' + link + filename + '.csv'
     # print savepath
     target_pd.to_csv(savepath, header=False)
@@ -304,28 +373,33 @@ def progress_test(counts, lenfile, speed):
     sys.stdout.write("""\r%d%%|%s|completed %d * projects|Speed : %.4f s/ projects|ETA: %s min""" %
                      (process * 100, hashes + spaces, counts, speed, ETA))
 
-    
     sys.stdout.flush()
 
-if __name__ == '__main__':
+
+def main(target, category, dates):
     gc.enable()
     requests_cache.install_cache(
         cache_name="googletrends_request_cache", backend="sqlite", expire_date=300)
 
     link = findoperation()
-    
 
     locals_file_path = os.path.split(os.path.realpath(__file__))[0]
-    target = locals_file_path + link + '1.txt'
+    target = locals_file_path + link + 'keywords.txt'
     keywords = ImportListInfobyFile(locals_file_path, target)
     makedirs('data', locals_file_path, link)
-
-    dates = chunks(DateProduce(2004), 3)
+    if dates == False:
+        dates = chunks(DateProduce(2004), 3)
+    else:
+        dates = dates
+    if category != 7:
+        category = category
+    else:
+        category = 7
 
     #keywords = ['FDEF', 'FFBC' ,'THFF' ,'FFIN' ,'FFNW']
     key = chunks(keywords, 1)
     uncompleted_keywords = keywords
-    
+
     # print key
     l_orign = len(key)
     l = l_orign
@@ -341,7 +415,7 @@ if __name__ == '__main__':
         while abnorl:
 
             try:
-                downlaod(dates, keywords, link, locals_file_path)
+                downlaod(dates, keywords, link, locals_file_path, category)
             except Exception as e:
                 print e
                 request_time = request_time + 1
@@ -376,3 +450,7 @@ if __name__ == '__main__':
         progress_test(l_orign - l, l_orign, speed)
 
     print 'download finished'
+
+
+if __name__ == '__main__':
+    main()
