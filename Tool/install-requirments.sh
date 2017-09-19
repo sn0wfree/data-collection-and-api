c58de664f80ef76e0e5950d0ced8e948428c9bc4
sn0wfree <snowfreedom0815@gmail.com>
2017-09-19 14:06:13 +0100

    change shell command

--- Tool/install-requirments.sh
+++ Tool/install-requirments.sh
@@ -5,9 +5,11 @@ GET_PIP_FILE=/tmp/get-pip.py
 GET_REQIREMENT_FILE =/tmp/requirements.txt
 
 # install pip
-#curl "https://bootstrap.pypa.io/get-pip.py" -o "${GET_PIP_FILE}"
-#python ${GET_PIP_FILE}
+curl "https://bootstrap.pypa.io/get-pip.py" -o "${GET_PIP_FILE}"
+python ${GET_PIP_FILE}
 
 # install requirements
-curl "https://raw.githubusercontent.com/sn0wfree/data-collection-and-api/master/API/Google_trend/Source/requirements.txt" -o "${GET_REQIREMENT_FILE}"
-pip install -r ${GET_REQIREMENT_FILE}
+#curl "https://raw.githubusercontent.com/sn0wfree/data-collection-and-api/master/API/Google_trend/Source/requirements.txt" -o "${GET_REQIREMENT_FILE}"
+pip install requests
+pip install requests_cache
+pip install pandas
