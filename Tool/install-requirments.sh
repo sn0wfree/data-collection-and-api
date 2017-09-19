2168ea7a5eb748de695d082d9d1df5fb7b71a51f
sn0wfree <snowfreedom0815@gmail.com>
2017-09-19 13:52:20 +0100

    sh2

--- Tool/install-requirments.sh
+++ Tool/install-requirments.sh
@@ -2,7 +2,12 @@
 # Install pip on Mac
 
 GET_PIP_FILE=/tmp/get-pip.py
+GET_REQIREMENT_FILE =/tmp/requirements.txt
 
 # install pip
 curl "https://bootstrap.pypa.io/get-pip.py" -o "${GET_PIP_FILE}"
 python ${GET_PIP_FILE}
+
+
+curl "https://raw.githubusercontent.com/sn0wfree/data-collection-and-api/master/API/Google_trend/Source/requirements.txt" -o "${GET_REQIREMENT_FILE}"
+pip install -r GET_REQIREMENT_FILE
