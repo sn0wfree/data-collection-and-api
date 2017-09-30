f4f1f5b5a306d343a10ac7fee2ce3cef27e26519
sn0wfree <snowfreedom0815@gmail.com>
2017-09-30 23:14:40 +0800

    test1

--- DataCollection/ChinaAidInfoProject/ChinaAid.py
+++ DataCollection/ChinaAidInfoProject/ChinaAid.py
@@ -11,64 +11,15 @@ try:
     from collections import ChainMap
 except ImportError:
     from chainmap import ChainMap
-import pandas as pd
 
 
-class APIrequestData():
+def InitalSqliteConnection(target):
+    if target != 0:
+        conn = sqlite3.connect(target)
+    else:
+        conn = sqlite3.connect(":memory:")
 
-    def __init__(self):
-        self.param_default = ['donor', 'status', 'intent', 'year', 'crs_sector_name',
-                              'flow_class', 'recipient_name', 'recipient_iso2', 'recipient_iso3']
-
-    def requestbyapi(self, param='Default'):
-        if param == 'Default':
-            self.req = requests.get(
-                'http://china.aiddata.org/aggregates/projects?get=%s' % (','.join(self.param_default)))
-        else:
-            self.req = requests.get(
-                'http://china.aiddata.org/aggregates/projects?get=%s' % (','.join(param)))
-
-        self.text = self.req.text
-        # print ','.join(param)
-
-    def Store(self):
-
-        self.dataframe = pd.DataFrame(
-            eval(self.text.replace('null', '"NULL"')))
-
-        self.dataframe.to_hdf('ChinaAid.h5', 'ChinaAid',
-                              complevel=9, complib='blosc')
-        #
-
-
-class sqlitecahche():
-
-    def __init__(self):
-        self.cache_conn = self.InitalSqliteConnection(target)
-
-    def InitalSqliteConnection(self, target):
-        if target != 0:
-            conn = sqlite3.connect(target)
-        else:
-            conn = sqlite3.connect(":memory:")
-
-        return conn
-
-
-class hdf5cache():
-
-    def __init__(self, name):
-        self = pd.io.pytables.HDFStore('%s-cache.hdf5' % name)
-
-        self.dataset_key = []
-
-    def add(self, *kwargs):
-        for key, value in kwargs.items():
-            self[key] = value
-        self.update_key()
-
-    def update_key(self):
-        self.dataset_key = self.cache.keys()
+    return conn
 
 
 class urllibcache(object):
@@ -108,13 +59,43 @@ class DataContainer():
 
 class Inhtml():
 
-    def __init__(self, src, header, publicaddress):
+    def __init__(self, src, header, publicaddress, AutoCollect=False):
         self.requesttext = requests.get(src, headers=header).text
         self.data = ChainMap()
-        self.publicaddress = publicaddress
         self.link = ChainMap()
+        self.publicaddress = publicaddress
+        self.AutoCollection(AutoCollect=AutoCollect)
+
+    def AutoCollection(self, AutoCollect=False):
+        if AutoCollect:
+            dict2 = {
+                'project_name': "//div[@class ='container main']//div[@class ='project-page ']//h1[@class ='project-header page-header']",
+                'project_brief': "//div[@class ='container main']//div[@class ='project-page ']//h1[@class ='project-header page-header']/small[@class]"}
+            self.multixpathparser(dict2)
+            pathstr = ["//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Donor']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Intent']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='StartPlanned']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='LoanType']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Transaction']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='ParticipatingOrganization']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Contact']/../*",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Description']",
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Geocode']",
+
+                       "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Capacity']"
+                       ]
+            [self.parser(xp) for xp in pathstr]
+            backup = lxml.etree.HTML(self.requesttext).xpath(
+                "//div[@class='container main']//div[@class='project-page ']//li//text()")
+            self.add({'Backup': [ws.strip()
+                                 for ws in backup if ws.strip() != '']})
+
+            self.HrefAdd(hrefxpth='default')
+        else:
+            pass
 
     def add(self, d):
+
         if isinstance(d, type(ChainMap())):
             self.data = self.data.new_child(d)
 
@@ -128,28 +109,51 @@ class Inhtml():
         else:
             raise ValueError, 'Unknown value'
 
-    def xpathparser_test(self, pathstr, variablename, addi=True):
-        x = lxml.etree.HTML(self.requesttext).xpath(pathstr)
-        if len(x) == 1:
-            if addi:
-                self.add({variablename: x[0].text.strip()})
-
-                print x[0].text.strip()
-
-            return (variablename, x[0].text.strip())
-        elif len(x) > 1 and addi:
-            temp = {variablename + 'of' + str(x.index(value)): value.text.strip()
-                    for value in x}
-            print temp.values()
-            if addi:
-                self.add(temp)
-            return temp
-
+    def HrefAdd(self, hrefxpth):
+        if hrefxpth == '' or hrefxpth == 'default':
+            hrefxpth = "//div[@class='container main']//div[@class='project-page ']//a[@href]"
         else:
-            print len(x)
-            if addi:
-                self.add({variablename: 'NULL'})
-            return (variablename, 'NULL')
+            pass
+
+        href = lxml.etree.HTML(self.requesttext).xpath(hrefxpth)
+        hrefstatusdict = []
+        hrefinsitelink = []
+        hrefinsitelinkbackup = []
+
+        for a in href:
+            try:
+                l = (a.text, a.attrib['href'])
+            except UnicodeEncodeError, e:
+                l = (a.text.encode('utf-8'), a.attrib['href'])
+
+            if l[0] != None and 'http' not in l[0] and 'More about this resource' not in l[0]:
+                # print l[0].encode('utf-8')
+                if l[1][0] == '/':
+                    hrefstatusdict.append((l[0], urlpath[:-2] + l[1]))
+                else:
+                    hrefstatusdict.append((l[0], urlpath + l[1]))
+
+            elif l[0] == None:
+
+                if l[1][0] == '/':
+                    hrefinsitelink.append(urlpath[:-2] + l[1])
+                else:
+                    hrefinsitelink.append(urlpath + l[1])
+
+            elif 'More about this resource' in l[0]:
+                if l[1][0] == '/':
+                    hrefinsitelink.append(urlpath[:-2] + l[1])
+                else:
+                    hrefinsitelink.append(urlpath + l[1])
+
+            elif 'http' in l[0]:
+                hrefinsitelinkbackup.append(l[0])
+
+        self.link['Status'] = dict(hrefstatusdict)
+        self.link['InSiteLink'] = hrefinsitelink
+        self.link['InSiteLinkBackup'] = hrefinsitelinkbackup
+        # print hrefinsitelink
+        # print hrefinsitelinkbackup
 
     def xpathparser(self, pathstr, variablename, addi=True):
         x = lxml.etree.HTML(self.requesttext).xpath(pathstr)
@@ -166,7 +170,7 @@ class Inhtml():
             return temp
 
         else:
-            print len(x)
+
             if addi:
                 self.add({variablename: 'NULL'})
             return (variablename, 'NULL')
@@ -180,39 +184,50 @@ class Inhtml():
         self.add(dict(t1))
         self.add(dict(t2))
 
-    def detectchild(self, t):
+    def detectchild(self, t, hrefstatus=False):
 
         if len(t.getchildren()) > 1:
+            text = [w.strip() for g in t.getchildren()
+                    for w in g.xpath('.//text()') if w.strip() != '']
+
             # print t.xpath('./*[starts-with(name(),'ul')]')
             #     print pr
-
-            href = [dict(g.text, t.attrib['href'])
-                    for g in t.getchildren() if g.tag == 'a' if 'href' in t.attrib.keys()]
-            for g in t.getchildren():
-                print g
-                if g.tag == 'a' and 'href' in g.attrib.keys():
-                    print g.attrib['href']
-                    print g.text
-                    print t.attrib['flaggable_type']
-
-            if href != []:
-                print href, "9"
-                self.link = self.link.new_child(href)
+            if hrefstatus:
+                href = {}
+                for g in t.getchildren():
+                    if g.tag == 'a' and 'href' in g.attrib.keys():
+                        try:
+                            href[text[0]] = (g.text, g.attrib['href'])
+                        except UnicodeEncodeError:
+                            href[text[0]] = (g.text.encode(
+                                'utf-8'), g.attrib['href'])
+                if href != {}:
+                    self.link = self.link.new_child(href)
             else:
-                print href, '8'
+                pass
 
-            return [w.strip() for g in t.getchildren() for w in g.xpath('.//text()') if w.strip() != '']
+            # href[g.text] = g.attrib['href']
+            # href.append(dict(g.text, g.attrib['href']))
 
         elif len(t.getchildren()) == 1:
-            href = [dict(t.text, t.attrib['href'])
-                    for g in t.getchildren() if 'href' in t.attrib.keys()]
-            if href != []:
-                print href, "9"
-                self.link = self.link.new_child(href)
+            text = [v.strip() for v in t.xpath(".//text()") if v.strip() != '']
 
-            return [v.strip() for v in t.xpath(".//text()") if v.strip() != '']
+            if hrefstatus:
+                href = {}
+                for g in t.getchildren():
+                    if g.tag == 'a' and 'href' in g.attrib.keys():
+                        try:
+                            href[text[0]] = (g.text, g.attrib['href'])
+                        except UnicodeEncodeError:
+                            href[text[0]] = (g.text.encode(
+                                'utf-8'), g.attrib['href'])
+                if href != {}:
+                    self.link = self.link.new_child(href)
+            else:
+                pass
         else:
-            return []
+            text = []
+        return text
 
     def list2dict(self, ls):
 
@@ -228,35 +243,35 @@ class Inhtml():
             value = 'NULL'
         return (key, value)
 
-    def parser(self, xpth, addi=True):
-        # xpth = "//div[@class ='container main']//div[@class ='project-page
-        # ']//li[@flaggable_type='Intent']/../*"
-        test2 = lxml.etree.HTML(self.requesttext).xpath(xpth)
+    def parser(self, xpth, addi=True, special=False):
 
-        chain2 = dict([self.list2dict(self.detectchild(t)) for t in test2])
+        var = xpth.split('@flaggable_type=')[-1].split("'")[1]
+        test2 = lxml.etree.HTML(self.requesttext).xpath(xpth)
+        if var not in ['Description', 'ParticipatingOrganization', 'Contact', 'Geocode']:
+            chain2 = dict([self.list2dict(self.detectchild(t)) for t in test2])
+        else:
+            if len(test2) == 1:
+                chain2 = {var: self.detectchild(test2[0])}
+            else:
+                print var
+                chain2 = {
+                    var: dict([self.list2dict(self.detectchild(t)) for t in test2])}
+
         if addi:
-            self.add(chain2)
+            if chain2 != {}:
+                self.add(chain2)
         else:
             return chain2
 
 
-if __name__ == '__main__':
-    requests_cache.install_cache(
-        cache_name="ChinaAidProjectInfo", backend="sqlite", expire_date=300000)
-    Data = DataContainer()
-
-    max_project_id = 2521
-    urlpath = 'http://china.aiddata.org/projects/'
-
-    for ident in xrange(max_project_id):
+def main(testurlpath, headers='default'):
+    if headers == 'default':
+        headers = {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+    else:
         pass
-    #-----------
 
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-
-    headers = {'User-Agent': user_agent}
-
-    request = requests.get(urlpath + str(1654), headers=headers)
+    request = requests.get(testurlpath, headers=headers)
 
     test = lxml.etree.HTML(request.text).xpath(
         "//div[@class='container main']/iframe")
@@ -264,70 +279,26 @@ if __name__ == '__main__':
         raise ValueError, 'iframe contain multi-information'
     elif 'src' in test[0].keys():
         srcframe = Inhtml(test[0].attrib['src'], headers,
-                          publicaddress='http://china.aiddata.org/')
-        print 'project_name'
+                          publicaddress='http://china.aiddata.org/', AutoCollect=True)
 
-        dict2 = {
-            'project_name': "//div[@class ='container main']//div[@class ='project-page ']//h1[@class ='project-header page-header']",
-            'project_brief': "//div[@class ='container main']//div[@class ='project-page ']//h1[@class ='project-header page-header']/small[@class]"}
-        srcframe.multixpathparser(dict2)
+    return srcframe.data, srcframe.link
 
-        print srcframe.data
-        print '--------'
-        print 'project_detail'
+if __name__ == '__main__':
+    requests_cache.install_cache(
+        cache_name="ChinaAidProjectInfo", backend="sqlite", expire_date=300000)
+    Data = DataContainer()
 
-        pathstr = ["//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Donor']/../*",
-                   "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Intent']/../*",
-                   "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='StartPlanned']/../*",
-                   "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='LoanType']/../*",
-                   "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Transaction']/../*",
+    urlpath = 'http://china.aiddata.org/projects/'
+    projectid = 2521
+    testurlpath = urlpath + str(projectid)
+    #--------
 
-                   "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Capacity']",
+    dataset = main(testurlpath)
+    print dataset[0]
 
+    #-----------
 
 
-                   ]
-        "//div[@class ='container main']//div[@class ='project-page ']//a[@href]"
-        pr = lxml.etree.HTML(srcframe.requesttext).xpath(
-            "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Intent']/../*")
-        print pr
-
-        for p in pr:
-
-            for ss in p.getchildren():
-                if ss.tag == 'a':
-
-                    if 'href' in ss.attrib.keys():
-                        print ss.text, ss.attrib['href']
-
-                # print ss.xpath('.//text()')
-            print '---------'
-            "//div[@class ='container main']//div[@class ='project-page ']//div[@id='resources']"
-            "//div[@class ='container main']//div[@class ='project-page ']//div[@id='geocodes']"
-
-        te1 = "//div[@class ='container main']//div[@class ='project-page ']//ul[@id='resources']/*"
-        te2 = "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Description']"
-        te3 = "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='ParticipatingOrganization']/../*"
-        te4 = "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Contact']/../*"
-        te5 = "//div[@class ='container main']//div[@class ='project-page ']//li[@flaggable_type='Geocode']"
-
-        [srcframe.parser(xp) for xp in pathstr]
-
-        backup = lxml.etree.HTML(srcframe.requesttext).xpath(
-            "//div[@class='container main']//div[@class='project-page ']//li//text()")
-        srcframe.add({'backup': [ws.strip()
-                                 for ws in backup if ws.strip() != '']})
-
-        for s in srcframe.data.keys():
-            if s != 'backup':
-                print s, ":", srcframe.data[s]
-                pass
-
-                # print 'flaggable_type'
-        print 'link:', srcframe.link
-
-    else:
-        pass
 '''
 
 
